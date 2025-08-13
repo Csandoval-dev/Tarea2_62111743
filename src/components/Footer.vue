d6fb5762d58a72358902224b18d6368f1c3fab60
Csandoval-dev <156733491+Csandoval-dev@users.noreply.github.com>
2025-08-13 11:38:05 -0600

    Modificacion en Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
 <template>
     <footer class="bg-dark mt-5 py-5">
         <div class="container-xl">
-            <p class="text-white text-center fs-4 mt-4 m-md-0">ArticulosSPS - 
+            <p class="text-white text-center fs-4 mt-4 m-md-0">LibreriaSPS - 
                 Todos los derechos Reservados
                 Por Carlos Sandoval</p>
         </div>
